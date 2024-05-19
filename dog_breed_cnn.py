b72bae45281b9550a45179c20906859a67b18e1b
lindaspellman <lindaspellman@live.com>
2024-05-18 23:40:51 -0600

    working cnn, but not high accuracy yet

--- dog_breed_cnn.py
+++ dog_breed_cnn.py
@@ -49,15 +49,19 @@ no_headers = no_headers.drop(no_headers.columns[[0,1,2,34,35,36,37,38]], axis=1)
 no_headers
 # figure out how to display no_headers without the numbered column names
 
-dog_breed_features = np.array(no_headers)
+#%%
+dog_breed_features = np.array(dog_breeds)
 
 #%%
-num_rows = len(no_headers)
+num_breeds = int(len(dog_breeds) / 2)
 breed_labels = {} 
-for i in range(num_rows): 
-    row = [0] * num_rows 
-    row[i] = 1 
-    breed_labels[i] = row 
+for i in range(num_breeds):
+    column = [0] * (num_breeds * 2)
+    column[i] = 1 
+    column[i + num_breeds] = 1
+    breed_labels[i] = column
+#%%
+breed_labels = pd.DataFrame(breed_labels)
 
 #%% 
 # Convert all non-integer values to integers
@@ -105,9 +109,9 @@ for i in range(num_rows):
 # Get our target variable and features and split them into test and train datasets
 X = dog_breeds[['a_adaptability'
                 ,'a1_adapts_well_to_apartment_living'
-                , 'a2_good_for_novice_owners'
-                , 'a3_sensitivity_level'
-                , 'a4_tolerates_being_alone'
+                ,'a2_good_for_novice_owners'
+                ,'a3_sensitivity_level'
+                ,'a4_tolerates_being_alone'
                 ,'a5_tolerates_cold_weather'
                 ,'a6_tolerates_hot_weather'
                 ,'b3_dog_friendly'
@@ -132,13 +136,20 @@ X = dog_breeds[['a_adaptability'
                 ,'both_family_kid_friendliness'
                 ,'size'
                 ]]
-y = dog_breeds['index']
-
+X.head()
+#%%
+y = breed_labels
+y.head()
+#%%
 from sklearn.preprocessing import StandardScaler
 
 # prob with data: only one example of each dog breed. Given testing data random inputs for dog breeds to be returned on?
 # need only one friendly column, ect, so it won't overwhelm size column. Or weight size more?
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+len(y)
+#%%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+len(X_train)
+#%%
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
@@ -150,20 +161,25 @@ X_test_scaled = scaler.transform(X_test)
 # Create the model and train it, use default hyperparameters for now
 # Define the model
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(64, activation='relu'), 
-    # input_shape=(X_train_scaled.shape[dog_breed_features.shape[1],1]),
-    tf.keras.layers.Dense(32, activation='relu'),
-    tf.keras.layers.Dense(dog_breed_features.shape[0], activation='sigmoid') # does sigmoid give good linear results?
+    tf.keras.layers.Dense(dog_breed_features.shape[1], activation='relu'), 
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(num_breeds, activation='softmax')
     ])
+# show to teacher and tutors - ask for help
+# explain how data is one sample per dog breed - 358 samples - train on that and have network converge on answering the question - doubled samples and split in half for training and testing
+# is background shuffling the problem? Or does it help?
 print('Model Defined')
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 print("Model Compiled")
 
+
+
+
+#%%
 # Train the model
-# model.fit(X_train, y_train, epochs=10, batch_size=358, validation_split=0.2)
-model.fit(dog_breed_features, breed_labels, epochs=10)
+model.fit(X_train, y_train, epochs=10, batch_size=num_breeds, validation_split=0.5)
 
 # model = XGBRegressor()
 # model.fit(X_train, y_train)
@@ -176,6 +192,9 @@ print(f'Test Accuracy: {accuracy}')
 # Make predictions
 predictions = model.predict(X_test)
 print("Predictions Made")
+
+
+
 
 # %%
 # print("Predictions Printed: ")
