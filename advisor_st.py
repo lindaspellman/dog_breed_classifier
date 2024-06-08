8e5fa3f109836b59e86244fc4c4cf00f2e4c34b6
lindaspellman <lindaspellman@live.com>
2024-06-08 00:18:03 -0600

    model outputs recs to streamlit ui

---
+++ advisor_st.py
@@ -0,0 +1,100 @@
+# streamlit run <filename>
+
+import os
+# import dog_breed_rfc
+# from dog_breed_rfc import predict_optimal_dog_breeds
+import joblib
+import numpy as np
+
+#%%
+try:
+    import streamlit as st 
+except ImportError:
+    os.system('pip install streamlit')
+    import streamlit as st
+
+# Load the model
+model = joblib.load('model.pkl')
+
+# function to run model with user input
+def predict_optimal_dog_breeds(size, num_dogs_had, home_size, alone_dogs, sensitivity, cold_weather, hot_weather, child_friendly, dog_friendly, stranger_friendly, shedding_amount, drooling_potential, groom_ease, weight_gain_potential, trainability, intelligence, mouthiness, prey_drive, barking, wanderlust_potential, en_lvl, intensity, ex_needs, playfulness):
+    data = np.array([[size, num_dogs_had, home_size, alone_dogs, sensitivity, cold_weather, hot_weather, child_friendly, dog_friendly, stranger_friendly, shedding_amount, drooling_potential, groom_ease, weight_gain_potential, trainability, intelligence, mouthiness, prey_drive, barking, wanderlust_potential, en_lvl, intensity, ex_needs, playfulness]])
+    prediction = model.predict(data)
+    return prediction
+    
+st.title('Dog Breed Advisor')
+st.write('Welcome to the Dog Breed Advisor! Here, would-be dog-owners can input details about their lifestyle conditions and receive recommendations of dog breeds that may fit with their lives.')
+st.write("My name is Linda Spellman. This website's functioning constitutes my senior project as Computer Science student at Brigham Young University - Idaho. The machine learning system uses RandomForestClassifier.")
+st.divider() 
+
+# size
+size = st.slider("On a scale of 1-5, what size dog do you want? 1. Tiny. 2. Small. 3. Medium. 4. Large. 5. Giant.",1,5,3)
+# a2_good_for_novice_owners
+num_dogs_had = st.slider('How many dogs have you had experienced ownership with by this point in your life? 0, 1, 2, 3, or 4+?', 0, 4, 2)
+if num_dogs_had == 0:
+    num_dogs_had = 1
+elif num_dogs_had == 1:
+    num_dogs_had = 2
+elif num_dogs_had == 2:
+    num_dogs_had = 3
+elif num_dogs_had == 3:
+    num_dogs_had = 4
+elif num_dogs_had == 4:
+    num_dogs_had = 5
+# a1_adapts_well_to_apartment_living
+home_size = st.slider("On a scale of 1-5, what size home do you live in? 1. Tiny Apartment (~400 square feet or less). 2. Apartment (~800 square feet). 3. Single-family home (~2000 square feet). 4. Large single-family home (~3500 square feet. 5. Mansion (at least 8000 square feet)", 1, 5, 3)
+# a4_tolerates_being_alone
+alone_dogs = st.slider("On a scale of 1-5, will you need to leave your dog alone for long periods of time?", 1, 5, 3)
+# a3_sensitivity_level
+sensitivity = st.slider("On a scale of 1-5, how sensitive a dog would you prefer? 1: Lowest sensitivity. 5: Highest sensitivity.", 1,5,3)
+# a5_tolerates_cold_weather
+cold_weather = st.slider("On a scale of 1-5, how cold is the coldest time of the year in your location?", 1, 5, 3)
+# a6_tolerates_hot_weather
+hot_weather = st.slider("On a scale of 1-5, how hot is the hottest time of the year in your location?",1,5,3)
+# both_family_kid_friendliness
+child_friendly = st.slider("On a scale of 1-5, how often will your dog be around children? 1 = Never. 2 = Rarely. 3 = Probably. 4 = Often. 5 = I want a good family dog for my children.", 1, 5, 3)
+# b3_dog_friendly
+dog_friendly = st.slider("Will your dog need to be friendly towards other dogs or guard against strange dogs? 1 = Friendly. 5 = Guarded. ", 1, 5, 3)
+# b4_friendly_toward_strangers
+stranger_friendly = st.slider("In the same vein of thinking, will your dog need to guard against strangers or be friendly towards strangers?", 1, 5, 3)
+# c1_amount_of_shedding
+shedding_amount = st.slider("On a scale of 1-5, would you prefer a non-shedding dog, a sometimes-shedding dog, or are you willing to clean up after an often-shedding dog (if it is an indoor dog)?", 1, 5, 3)
+# c2_drooling_potential
+drooling_potential = st.slider("On a scale of 1-5, would you prfer a dog who doesn't drool or are you fine with drooling?",1,5,3)
+# c3_easy_to_groom
+groom_ease = st.slider("On a scale of 1-5, would you prefer an easy-to-groom dog, are you planning to professionally groom your dog, or are you willing to groom a difficult-to-groom dog at home?", 1, 5, 3)
+# c5_potential_for_weight_gain
+weight_gain_potential = st.slider("Some dogs require feeding on a schedule or they will gain weight easily. On a scale of 1 to 5, would you prefer a dog who can regulate their eating schedule or are you disciplined enough to keep to a schedule for a dog who has a high potential for weight gain?", 1, 5, 3)
+# d1_easy_to_train
+trainability = st.slider("On a scale of 1-5, how trainable a dog would you prefer. 1: Challenging. 5: Easy to train.", 1, 5, 3)
+# d2_intelligence
+intelligence = st.slider("On a scale of 1-5, do you want a more intelligent dog, or are you fine with a less intelligent dog? 1: Dumb dog. 5: Very intelligent dog.", 1, 5,3)
+# d3_potential_for_mouthiness
+mouthiness = st.slider("On a scale of 1-5, would you prefer a naturally non-mouthy dog or are you willing to put up with mouthiness/train your dog to not be mouthy?",1,5,3)
+# d4_prey_drive
+prey_drive = st.slider("On a scale of 1-5, do you want a dog who is given to a high prey drive and will easily give chase, or do you want a dog who does not care and wants to stay home? 1: Low prey drive. 5: High prey drive.", 1, 5, 3)
+# d5_tendency_to_bark_or_howl
+barking = st.slider("On a scale of 1-5, would you prefer a quiet dog or a loud dog? 1: Not prone to barking/howling. 5: Very prone to vocalizing.",1,5,3)
+# d6_wanderlust_potential
+wanderlust_potential = st.slider("On a scale of 1-5, how comfortable with a dog having a high wanderlust potential? 1: Prefers little wanderlust. 5: Comfortable with higher wanderlust. ", 1,5,3)
+# e1_energy_level
+en_lvl = st.slider("On a scale of 1-5, do you need a high-energy dog or a low-energy dog? High-energy dogs are better suited to being working dogs, while low-energy dogs are often couch potatos. 1: Low-energy. 5: High-energy",1,5,3)
+# e2_intensity
+intensity = st.slider("On a scale of 1-5, would you prefer a high-intensity dog or low-intensity dog? High-intensity dogs are energetic and enthusiastic in all their activities, while low-intensity dogs are more subdued and quiet. 1: Low-intensity. 5: High-intensity.", 1, 5, 3)
+# e3_exercise_needs
+ex_needs = st.slider("On a scale of 1-5, how willing are you to spend a lot of time walking your dog? Would you prefer short periods of exercise or are you willing to walk your dog for longer periods and further distances? 1: Prefer short walks. 5: Comfortable with long walks.", 1,5,3)
+# e4_potential_for_playfulness
+playfulness = st.slider("On a scale of 1-5, how interested are you in playing with your dog and how much time do you have to spend playing with your dog? Would you prefer a more playful dog or less playful dog? 1. Less playful. 5: More playful.",1,5,3)
+
+breed_recs = []
+
+submitted = st.button('Submit', key=1)
+if submitted:
+    st.write(f'Your top 5 dog breed recommendations for your lifestyle conditions are: ')
+    # send all answers to the model
+    for i in range(0,4):
+        breed_rec = predict_optimal_dog_breeds(size, num_dogs_had, home_size, alone_dogs, sensitivity, cold_weather, hot_weather, child_friendly, dog_friendly, stranger_friendly, shedding_amount, drooling_potential, groom_ease, weight_gain_potential, trainability, intelligence, mouthiness, prey_drive, barking, wanderlust_potential, en_lvl, intensity, ex_needs, playfulness)
+        breed_recs.append(breed_rec)
+        # if any of the recs in the list are the same, rerun the model. Basically, I want a set of unique recs.
+    for rec in breed_recs:
+        st.write(f'{rec}')
--- dog_breed_rfc.py
+++ dog_breed_rfc.py
@@ -1,4 +1,5 @@
 # %%
+# library importing cell
 # RandomForest classifier - small amount of columns - need better accuracy than random guessing
 # transformer
 # SVM - support vector machine 
@@ -11,12 +12,29 @@ import numpy as np
 import joblib
 
 #%%
-dog_breeds = pd.read_csv('C:\\Users\\Linda\\OneDrive\\Desktop\\BYUI\\2024_Spring_Senior_Project\\dog_breed_classifier\\dataset\\breeds.csv')
+# dog_breeds = pd.read_csv('C:\\Users\\Linda\\OneDrive\\Desktop\\BYUI\\2024_Spring_Senior_Project\\dog_breed_classifier\\dataset\\breeds.csv')
+synthetic_dog_breeds = pd.read_csv('C:\\Users\\Linda\\OneDrive\\Desktop\\BYUI\\2024_Spring_Senior_Project\\dog_breed_classifier\\dataset\\tabular-actgan-dog-breeds.csv')
+
+orig_dog_breeds = pd.read_csv('C:\\Users\\Linda\\OneDrive\\Desktop\\BYUI\\2024_Spring_Senior_Project\\dog_breed_classifier\\dataset\\breeds_to_change.csv')
+
+dog_breeds = pd.concat([orig_dog_breeds, synthetic_dog_breeds], ignore_index=True)
 
 dog_breeds['both_family_kid_friendliness'] = dog_breeds[['b1_affectionate_with_family','b2_incredibly_kid_friendly_dogs']].mean(axis=1)
 
+# Fill NaN values with 0
+dog_breeds['size'] = dog_breeds['size'].fillna(3)
+dog_breeds['both_family_kid_friendliness'] = dog_breeds['both_family_kid_friendliness'].fillna(3)
+
+# dog_breeds.dropna()
+
+dog_breeds['size'] = dog_breeds['size'].astype(int)
+dog_breeds['both_family_kid_friendliness'] = dog_breeds['both_family_kid_friendliness'].astype(int)
+
 #%%
+# dog_breeds.info()
+# dog_breeds['size'].unique()
 # dog_breeds.head()
+len(dog_breeds.columns)
 
 ##########################################################################################
 
@@ -37,60 +55,59 @@ dog_breeds['both_family_kid_friendliness'] = dog_breeds[['b1_affectionate_with_f
 
 
 # %%
-X = dog_breeds[[#'a_adaptability'
+X = dog_breeds[[##'a_adaptability'
                 # ,
                 'a1_adapts_well_to_apartment_living'
                 ,'a2_good_for_novice_owners'
                 ,'a3_sensitivity_level'
                 ,'a4_tolerates_being_alone'
-                ,'a5_tolerates_cold_weather'
+                ,
+                'a5_tolerates_cold_weather'
                 ,'a6_tolerates_hot_weather'
                 ,'b3_dog_friendly'
                 ,'b4_friendly_toward_strangers'
-                # ,'c_health_grooming'
-                ,'c1_amount_of_shedding'
+                ## ,'c_health_grooming'
+                ,
+                'c1_amount_of_shedding'
                 ,'c2_drooling_potential'
                 ,'c3_easy_to_groom'
-                ,'c4_general_health'
+                # ,'c4_general_health'
                 ,'c5_potential_for_weight_gain'
-                #,'d_trainability'
-                ,'d1_easy_to_train'
+                ##,'d_trainability'
+                ,
+                'd1_easy_to_train'
                 ,'d2_intelligence'
                 ,'d3_potential_for_mouthiness'
                 ,'d4_prey_drive'
                 ,'d5_tendency_to_bark_or_howl'
                 ,'d6_wanderlust_potential'
-                # ,'e_exercise_needs'
-                ,'e1_energy_level'
+                ## ,'e_exercise_needs'
+                ,
+                'e1_energy_level'
                 ,'e2_intensity'
                 ,'e3_exercise_needs'
                 ,'e4_potential_for_playfulness'
-                ,'both_family_kid_friendliness'
+                ,
+                'both_family_kid_friendliness'
                 ,'size'
                 ]]
-#%%
-X.head()
-#%%
+
+# X.head()
+
 # y = breed_labels
 y = dog_breeds['breed']
 
 #%%
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
-
-
-
-
+len(X.columns)
 #%%
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 # Initialize the Random Forest classifier
-rf_classifier = RandomForestClassifier(n_estimators=256, random_state=42)
+rf_classifier = RandomForestClassifier(n_estimators=256, random_state=42, warm_start=True)
 
 # Train the model
 rf_classifier.fit(X_train, y_train)
 
-
-
-
-# %%
 # Make predictions on the test set
 y_pred = rf_classifier.predict(X_test)
 
@@ -115,5 +132,34 @@ print(classification_report(y_test, y_pred))
 print('Confusion Matrix:')
 print(confusion_matrix(y_test, y_pred))
 
+# %%
+# len(y_pred)
+predictions = pd.DataFrame(y_pred)
+predictions#.head()
+#%%
+# dog_breeds_orig = pd.read_csv('C:\\Users\\Linda\\OneDrive\\Desktop\\BYUI\\2024_Spring_Senior_Project\\dog_breed_classifier\\dataset\\breeds_original.csv')
+
+# dog_breeds_orig['both_family_kid_friendliness'] = dog_breeds_orig[['b1_affectionate_with_family','b2_incredibly_kid_friendly_dogs']].mean(axis=1)
+
+# len(dog_breeds)
+#%%
+# half_num_rows = num_rows // 2
+# dog_breeds_first_half = dog_breeds.iloc[:half_num_rows]
+# dog_breeds_first_half
+
+#%%
+# Select the first half of the rows
+# dog_breeds_first_half = dog_breeds.drop_duplicates()
+# dog_breeds_first_half 
+#%%
+# dog_breeds['predictions'] = y_pred 
+dog_breeds['predictions'] = predictions 
+# dog_breeds_orig.head()
+dog_breeds[['breed','predictions']]
+# NEED TO FIX NANS IN predictions!!!
+
+#%%
+# Showing which predictions were correct
+dog_breeds[dog_breeds['breed'] == dog_breeds['predictions']]
 # %%
 joblib.dump(rf_classifier, 'model.pkl')
